d23463faa76bb3a57a3ce861d4ea7b9b4a45717b
brysonwaisi <brysonnyamwange@gmail.com>
2023-03-19 21:54:45 +0300

    feat: Create Navbar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
-import Nav from './components/navbar/Nav'
+import { Routes, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import { Navbar } from './components/Navbar'
+import {Home} from './pages/Home'
+import {About} from './pages/About'
+import { Store } from './pages/Store'
 
 function App() {
 
   return (
     <>
-      <Nav />
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
     </>
   )
 }
