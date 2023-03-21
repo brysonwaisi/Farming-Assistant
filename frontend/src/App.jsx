7528924a09c739fbe921fc4cf08d96d27d3e9d99
brysonwaisi <brysonnyamwange@gmail.com>
2023-03-21 21:44:01 +0300

    Complete frontend implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,13 @@ import Product from './components/Product'
 import ProductList from './pages/ProductList'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import Cart from './pages/Cart'
 
 function App() {
 
   return (
     <>
-      <Login />
+      <Cart />
     </>
   )
 }
