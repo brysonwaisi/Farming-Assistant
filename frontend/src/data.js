f9c84cd8a56e7a1d0c0fe264e6f786169dc39c6b
brysonwaisi <brysonnyamwange@gmail.com>
2023-03-21 14:12:15 +0300

    feat: Image Slider

---
+++ frontend/src/data.js
@@ -0,0 +1,78 @@
+
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://g2h4v2c9.stackpathcdn.com/themes/agrarium/wp-content/uploads/2021/08/Layer-850-300x233.png",
+    title: "HOT SALE",
+    desc: "DON'T COMPROMISE ON QUALITY.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://g2h4v2c9.stackpathcdn.com/themes/agrarium/wp-content/uploads/2021/08/Layer-851-1-300x233.png",
+    title: "IRRESISTABLE COMBINATION",
+    desc: "GET FLAT 30% OFF SALE.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://g2h4v2c9.stackpathcdn.com/themes/agrarium/wp-content/uploads/2021/09/Layer-594-2-300x233.png",
+    title: "FRESH BEEF",
+    desc: "DON'T COMPROMISE ON QUALITY! GET THE BEST DEALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.hsph.harvard.edu%2Fnutritionsource%2Fwp-content%2Fuploads%2Fsites%2F30%2F2012%2F09%2Fvegetables-and-fruits-farmers-market.jpg&tbnid=ngxtkc-mF50qpM&vet=12ahUKEwi2iZLq8Oz9AhWBrUwKHVCiBSgQMygCegUIARDjAQ..i&imgrefurl=https%3A%2F%2Fwww.hsph.harvard.edu%2Fnutritionsource%2Fwhat-should-you-eat%2Fvegetables-and-fruits%2F&docid=3KdI980rHs9GtM&w=425&h=282&q=vegetables&ved=2ahUKEwi2iZLq8Oz9AhWBrUwKHVCiBSgQMygCegUIARDjAQ",
+    title: "VEGGIES",
+  },
+  {
+    id: 2,
+    img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.world-grain.com%2Fext%2Fresources%2F2022%2F05%2F10%2FCereal-grains_cr-Adobe-Stock_E.jpg%3Fheight%3D667%26t%3D1652190823%26width%3D1080&tbnid=k5vkuRGc0y-0kM&vet=12ahUKEwidyvX28Oz9AhVEmicCHc9rBdkQMygBegUIARDgAQ..i&imgrefurl=https%3A%2F%2Fwww.world-grain.com%2Farticles%2F16880-fao-projects-year-on-year-decline-in-cereals-trade&docid=mOsuhtxN0Z-6vM&w=1001&h=667&q=cereals&ved=2ahUKEwidyvX28Oz9AhVEmicCHc9rBdkQMygBegUIARDgAQ",
+    title: "CEREALS",
+  },
+  {
+    id: 3,
+    img: "https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-3foodgroups_fruits_detailfeature.jpg?sfvrsn=64942d53_4",
+    title: "FRUITS",
+  },
+];
+
+export const popularProducts = [
+  {
+    id:1,
+    img:"/images/banana.jpeg",
+  },
+  {
+    id:2,
+    img:"/images/cabbage.jpeg",
+  },
+  {
+    id:3,
+    img:"/images/carrot.jpeg",
+  },
+  {
+    id:4,
+    img:"/images/cereals.jpeg",
+  },
+  {
+    id:5,
+    img:"/images/groundnuts.jpeg",
+  },
+  {
+    id:6,
+    img:"/images/honey.png",
+  },
+  {
+    id:7,
+    img:"/images/potatoes.jpeg",
+  },
+  {
+    id:8,
+    img:"/images/spices.jpeg",
+  }
+]
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Welcome from '../components/Welcome'
 import Navbar from '../components/Navbar';
+import Slider from '../components/Slider';
 
 const Home = () => {
   return (
     <div>
       <Welcome />
       <Navbar />
+      <Slider />
     </div>
   )
 }
