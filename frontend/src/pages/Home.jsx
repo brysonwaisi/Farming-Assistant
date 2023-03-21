ef6c3d5e19fe998db5e857038f77d68a2ef5b5fb
brysonwaisi <brysonnyamwange@gmail.com>
2023-03-21 13:45:42 +0300

    feat: Update Navbar

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Welcome from '../components/Welcome'
+import Navbar from '../components/Navbar';
+
+const Home = () => {
+  return (
+    <div>
+      <Welcome />
+      <Navbar />
+    </div>
+  )
+}
+
+export default Home;
