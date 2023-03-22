d461167a8d4ab5452800b5a466ad4edcea5d83bd
brysonwaisi <brysonnyamwange@gmail.com>
2023-03-22 11:55:12 +0300

    Update permissions

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { default: mongoose } = require('mongoose');
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 
 dotenv.config()
 
@@ -11,9 +12,11 @@ mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log('DB CONNECTION SUCCESSFUL'))
 .catch((err) => {
   console.log(err)
-})
+});
 
+app.use(express.json())
+app.use("/api/users", userRoute)
 
 app.listen(5000, () => {
   console.log(`Backend is running on ${port}`)
-})
+});
